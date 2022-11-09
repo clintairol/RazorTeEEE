287961fc96f96e56c9ea5992e5ebc9c3047d8b23
ACT-STUDENT <ACT-STUDENT@DESKTOP-SV8DJ3G>
2022-11-08 18:53:50 -0800

    Add project files.

---
+++ RazorTees/Models/Tees.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace RazorTees.Models
+{
+    public class Tees
+    {
+        public int Shirt_ID { get; set; }
+        public string ProductName { get; set; }
+        [DataType(DataType.Date)]
+   
+    }
+}
+
+
