80d876b93c46a65f7cfbf1c32f7ed432d0916ac0
Clint <sepalonclintairol@gmail.com>
2022-11-11 15:41:58 +0800

    Seeded arranged

--- RazorTees/Models/SeedData.cs
+++ RazorTees/Models/SeedData.cs
@@ -2,7 +2,11 @@
 using Microsoft.Extensions.DependencyInjection;
 using RazorTees.Data;
 using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
+using System.Security.Policy;
+
 namespace RazorTees.Models
 {
     public class SeedData
@@ -20,20 +24,59 @@ namespace RazorTees.Models
                 context.Tees.AddRange(
                     new Tees
                     {
+                        Product = "Tshirt",
+                        Quantity = 12,
+                        Brand = "Penshoppe",
+                        Color = "Red",
+                        Design = "Plain",
+                        Sizes = "S",
+                        Neckline = "V-shape",
+                        Material = "Linen",
+                        Price = 3.99,
+                        Date = DateTime.Now
+
 
                     },
                      new Tees
                      {
+                         Product = "Tshirt",
+                         Quantity = 12,
+                         Brand = "Penshoppe",
+                         Color = "Red",
+                         Design = "War is Over",
+                         Sizes = "S",
+                         Neckline = "V-shape",
+                         Material = "Linen",
+                         Price = 23.99,
+                         Date = DateTime.Now
 
                      },
                       new Tees
                       {
-
+                          Product = "Tshirt",
+                          Quantity = 12,
+                          Brand = "Bench",
+                          Color = "Blue",
+                          Design = "Plain",
+                          Sizes = "l",
+                          Neckline = "V-shape",
+                          Material = "Linen",
+                          Price = 8.99,
+                          Date = DateTime.Now
                       },
-                       new Tees
-                       {
-
-                       }
+                      new Tees
+                      {
+                         Product = "Tshirt",
+                        Quantity = 12,
+                        Brand = "Penshoppe",
+                        Color = "Baige",
+                        Design = "Plain",
+                        Sizes = "XL",
+                        Neckline = "Round-shape",
+                        Material = "Linen",
+                        Price = 6.99,
+                        Date = DateTime.Now
+                      }
                     );
                 context.SaveChanges();
             }
