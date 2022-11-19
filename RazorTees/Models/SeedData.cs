7a336e6970426c47bf51ef8ae9c0873e8e6be64f
TXmotheeeeee <itsgeligtimothy@gmail.com>
2022-11-19 18:22:26 +0800

    my items initials

--- RazorTees/Models/SeedData.cs
+++ RazorTees/Models/SeedData.cs
@@ -9,78 +9,38 @@ using System.Security.Policy;
 
 namespace RazorTees.Models
 {
-    public class SeedData
-    {
-        public static void Initialize(IServiceProvider serviceProvider)
+        public class SeedData
         {
-            using ( var context = new RazorTeesContext(serviceProvider.GetRequiredService<DbContextOptions<RazorTeesContext>>()))
+            public static void Initialize(IServiceProvider serviceProvider)
             {
-                //LOOKING FOR T-SHIRTS OR APPARELS
-                if (context.Tees.Any())
+                using (var context = new RazorTeesContext(serviceProvider.GetRequiredService<DbContextOptions<RazorTeesContext>>()))
                 {
-                    return;
-                }
-
-                context.Tees.AddRange(
-                    new Tees
+                    //LOOKING FOR T-SHIRTS OR APPARELS
+                    if (context.Tees.Any())
                     {
-                        Product = "Tshirt",
-                        Quantity = 12,
-                        Brand = "Penshoppe",
-                        Color = "Red",
-                        Design = "Plain",
-                        Sizes = "S",
-                        Neckline = "V-shape",
-                        Material = "Linen",
-                        Price = 3.99,
-                        Date = DateTime.Now
+                        return;
+                    }
+
+                    context.Tees.AddRange(
+                        new Tees
+                        {
+                            Product = "Tshirt",
+                            Quantity = 12,
+                            Brand = "Penshoppe",
+                            Color = "Red",
+                            Design = "Plain",
+                            Sizes = "S",
+                            Neckline = "V-shape",
+                            Material = "Linen",
+                            Price = 3.99,
+                            Date = DateTime.Now
 
 
-                    },
-                     new Tees
-                     {
-                         Product = "Tshirt",
-                         Quantity = 12,
-                         Brand = "Penshoppe",
-                         Color = "Red",
-                         Design = "War is Over",
-                         Sizes = "S",
-                         Neckline = "V-shape",
-                         Material = "Linen",
-                         Price = 23.99,
-                         Date = DateTime.Now
-
-                     },
-                      new Tees
-                      {
-                          Product = "Tshirt",
-                          Quantity = 12,
-                          Brand = "Bench",
-                          Color = "Blue",
-                          Design = "Plain",
-                          Sizes = "l",
-                          Neckline = "V-shape",
-                          Material = "Linen",
-                          Price = 8.99,
-                          Date = DateTime.Now
-                      },
-                      new Tees
-                      {
-                         Product = "Tshirt",
-                        Quantity = 12,
-                        Brand = "Penshoppe",
-                        Color = "Baige",
-                        Design = "Plain",
-                        Sizes = "XL",
-                        Neckline = "Round-shape",
-                        Material = "Linen",
-                        Price = 6.99,
-                        Date = DateTime.Now
-                      }
-                    );
-                context.SaveChanges();
+                        }
+                        );
+                    context.SaveChanges();
+                }
             }
-        }
 
-    }
+        }
 }
